f21ea47b8cfa24362e93614e6e35b8b836295517
dimi <ixydimitrov@gmail.com>
2025-02-02 22:11:13 +0100

    join + cwd()

--- app/components/Typer/server.ts
+++ app/components/Typer/server.ts
@@ -11,7 +11,7 @@ async function retrieveTextFromDb(language: SUPPORTED_LANGUAGES) {
     Python: 'app/server/db/python.txt',
     Rust: 'app/server/db/rust.txt'
   }
-  const file = await fs.promises.readFile(path.resolve(db[language]), 'utf8');
+  const file = await fs.promises.readFile(path.join(process.cwd(), db[language]), 'utf8');
   const textList = file.split('\n');
   const textSample = []
   for (let i = 0; i < 5; i++) {
