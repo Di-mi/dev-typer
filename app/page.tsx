b460abbc2320dbb19ef917232b4dac115063124d
dimi <ixydimitrov@gmail.com>
2025-02-02 21:44:33 +0100

    added suspense

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@
 import Typer from "./components/Typer/Typer";
 
 import {InfoModal } from "./components/InfoModal";
-
+import { Suspense } from "react";
 
 export default async function App() {
   return (
     <div>
       <Typer  />
-      <InfoModal/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <InfoModal/>
+      </Suspense>
     </div>
   )
 
