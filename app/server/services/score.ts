32911035079bd815ce36a9e48fe7f35c87806f6a
dimi <ixydimitrov@gmail.com>
2025-03-23 09:34:04 +0100

    fixing type issue

--- app/server/services/score.ts
+++ app/server/services/score.ts
@@ -3,11 +3,17 @@
 import { db } from "../db"
 import { scoresTable } from "../db/schema"
 
-type createScoreType = typeof scoresTable.$inferInsert;
+type createScoreType = typeof scoresTable.$inferInsert & {
+    assigmentText: string
+    typedText: string
+    mistakesCounts: string
+    mistakesPercentage: string
+};
+
 
 export async function recordScore(score: createScoreType) {
     await db
-    .insert(scoresTable)
-    .values(score)
-  
+        .insert(scoresTable)
+        .values(score)
+
 }
